065c7057727126e3c4c2ec2a2fd43a19539dd3cb
rednas174 <sander987@live.nl>
2021-01-15 13:05:36 +0100

    Reformatted for better compatibility and readability
    For recursion, having a function that can be called with a command is very effective.

--- app.py
+++ app.py
@@ -7,23 +7,17 @@ Created on Fri Jan  8 15:39:32 2021
 
 #==================================================================#
 # TODO:                                                            #
-#   |                                                              #
-#   +-> Add /UwUify and /updatelog                                 #
+#    +-> Add /poll                                                 #
+#    +-> Add /WTFact                                               #
+#    +-> fix the big F                                             #
+#    +-> Emojify text (supercringe)                                #
 #==================================================================#
-
-# The app requires a specific message 
-#{
-#  "data":[
-#      {
-#       "message": "<Message here>"
-#      }
-#   ]
-#}
 
 from flask import Flask, request
 
 import bot.handlers.dnd_handler as dnd_handler
 import bot.handlers.misc_handler as misc_handler
+import bot.handlers.poll_handler as poll_handler
 import bot.utils as utils
 
 
@@ -31,8 +25,88 @@ app = Flask(__name__)
 
 # Main function called when the phone sends a request.
 # Whatever is returned from this function is whatever will be send back to the phone.
+
+# Variable for tracking the pipeline iterations
+command_recursions = 0
+def handle_command(command:str):
+    global command_recursions
+    
+    items = command.split(" ", 1)
+    
+    # Check for the necessity of recursion
+    if len(items) == 2:
+        if items[1][0] == "/":
+            command_recursions += 1
+            
+            # A maximum of 100 recursions
+            if command_recursions < 100:
+                
+                # re-evaluate the second part of the command, then replace the output with the current command and evaluate that.
+                command = items[0] + " " + handle_command(items[1])
+            else:
+                return "Sorry, too many recursions..."
+    
+    try:
+        if   command[:5]    == "/help":
+            returned_message = misc_handler.get_help(command)
+        
+        elif command[:5]    == "/roll":
+            returned_message = dnd_handler.roll_dice(command)
+            
+        elif command[:5]    == "/link": 
+            returned_message = misc_handler.get_group_link(command)
+    
+        elif command[:17]   == "/create_character":
+            returned_message = dnd_handler.create_character(command)
+        
+        elif command[:7]    == "/repeat":
+            returned_message = misc_handler.get_repeated_command(command)
+        
+        elif command[:10]   == "/horoscope":
+            returned_message = misc_handler.get_horoscope(command)
+
+        elif command[:4]    == "/bms":
+            returned_message = misc_handler.get_bms_script(command)
+
+        elif command[:6]    == "/agree":
+            returned_message = misc_handler.agree(command)
+
+        elif command[:9]    == "/disagree":
+            returned_message = misc_handler.disagree(command)
+        
+        elif command[:10]   == "/updatelog":
+            returned_message = misc_handler.get_updates(command)
+        
+        elif command[:7]    == "/UwUify":
+            returned_message = misc_handler.UwUify(command)
+        
+        elif command[:11]   == "/disclaimer":
+            returned_message = misc_handler.get_disclaimer(command)
+        
+        elif command[:2]    == "/f":
+            returned_message = misc_handler.F(command)
+        
+        elif command[:5]    == "/poll":
+            returned_message = poll_handler.handle_poll(command)
+        
+        elif command[:6]    == "/roast":
+            returned_message = misc_handler.get_roast(command)
+            
+        else: 
+            returned_message = "_Error 404: Command '" + command + "' not found. \n\nIf this is caused by a MASSIVE FUCKING LONG ASS TEXT, plz shorten it a bit. I won't break just cuz y'all tryna to ;)_"
+
+    except Exception as e:
+        print(e)
+        return "Oops, something went wrong, type '/roll help' for info about the syntax"
+    return returned_message
+
+
 @app.route('/', methods=['POST'])
 def result():
+    global command_recursions
+    command_recursions = 0
+    
+    returned_message = ""
     
     # Get metadata from message
     data_from_client = request.get_json()
@@ -41,52 +115,12 @@ def result():
     print(">>>", data_from_client["senderMessage"][:100].replace("\n", " -"))
     
     if len(data_from_client["senderMessage"]) > 10000:
-        return "Suck a huge fat cock, I'm not gonna repeat some _*huge mega uber triple style ginormous*_ bullshit spam"
+        returned_message = "Suck a huge fat cock, I'm not gonna repeat some _*huge mega uber triple style ginormous*_ bullshit spam"
     
-    try:
-        if data_from_client['senderMessage'][:5]    == "/help":
-            return utils.gen_send_data(misc_handler.get_help(data_from_client["senderMessage"]))
-        
-        elif data_from_client['senderMessage'][:5]  == "/roll":
-            return utils.gen_send_data(dnd_handler.roll_dice(data_from_client["senderMessage"]))
-            
-        elif data_from_client['senderMessage'][:5]  == "/link": 
-            return utils.gen_send_data(misc_handler.get_group_link(data_from_client["senderMessage"]))
+    returned_message = handle_command(data_from_client["senderMessage"])
     
-        elif data_from_client['senderMessage'][:17] == "/create_character":
-            return utils.gen_send_data(dnd_handler.create_character(data_from_client["senderMessage"]))
-        
-        elif data_from_client['senderMessage'][:7]  == "/repeat":
-            return utils.gen_send_data(misc_handler.get_repeated_command(data_from_client['senderMessage']))
-        
-        elif data_from_client['senderMessage'][:10] == "/horoscope":
-            return utils.gen_send_data(misc_handler.get_horoscope(data_from_client['senderMessage']))
-
-        elif data_from_client['senderMessage'][:4]  == "/bms":
-            return utils.gen_send_data(misc_handler.get_bms_script(data_from_client['senderMessage']))
-
-        elif data_from_client['senderMessage'][:6]  == "/agree":
-            return utils.gen_send_data(misc_handler.agree(data_from_client['senderMessage']))
-
-        elif data_from_client['senderMessage'][:9]  == "/disagree":
-            return utils.gen_send_data(misc_handler.disagree(data_from_client['senderMessage']))
-        
-        elif data_from_client['senderMessage'][:10] == "/updatelog":
-            return utils.gen_send_data(misc_handler.get_updates(data_from_client['senderMessage']))
-        
-        elif data_from_client['senderMessage'][:7]  == "/UwUify":
-            return utils.gen_send_data(misc_handler.UwUify(data_from_client['senderMessage']))
-        
-        elif data_from_client['senderMessage'][:11] == "/disclaimer":
-            return utils.gen_send_data(misc_handler.get_disclaimer(data_from_client['senderMessage']))
-        
-        elif data_from_client['senderMessage'][:2]  == "/f":
-            return utils.gen_send_data(misc_handler.F(data_from_client['senderMessage']))
-
-    except Exception as e:
-        print(e)
-        return utils.gen_send_data("Oops, something went wrong, type '/roll help' for info about the syntax")
-    return ""
+    return utils.gen_send_data(returned_message)
+    
         
         
 app.run(host = "0.0.0.0")
